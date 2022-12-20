9c2403b7957f18b84a6fca32e47065604b2601ef
Fırat Özden <31664778+fir4tozden@users.noreply.github.com>
2022-12-21 01:45:48 +0300

    Update g_ads4fb41c4f.js

--- g_ads4fb41c4f.js
+++ g_ads4fb41c4f.js
@@ -9,4 +9,4 @@
     <div class="google-ads" data-client-id="ca-pub-3188959633775259" data-ad-id="3261049626"></div>
 */
 
-$(function(){$(".google-ads").each((a,d)=>{$(d).html('<ins class="adsbygoogle" style="display:block;background-color:#C5C5C5" data-ad-client="'+$(d).data("client-id")+'" data-ad-slot="'+$(d).data("ad-id")+'" data-ad-format="auto" data-full-width-responsive="true"></ins><script>(adsbygoogle=window.adsbygoogle||[]).push({});</script>')})});
+$(function(){$(".google-ads").each((a,d)=>{$(d).removeClass('google-ads');$(d).html('<ins class="adsbygoogle" style="display:block;background-color:#C5C5C5" data-ad-client="'+$(d).data("client-id")+'" data-ad-slot="'+$(d).data("ad-id")+'" data-ad-format="auto" data-full-width-responsive="true"></ins><script>(adsbygoogle=window.adsbygoogle||[]).push({});</script>')})});
