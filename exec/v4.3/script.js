bf556912fc40293a67b1834b1b42f08cbd49e652
Fırat Özden <31664778+fir4tozden@users.noreply.github.com>
2023-08-26 04:44:12 +0300

    Update and rename script.js to script.js

--- exec/v4.3/script.js
+++ exec/v4.3/script.js
@@ -80,7 +80,7 @@ function ping() {
 setTimeout(ping, 5000);
 
 setInterval(() => {
-  document.querySelector("#uptime").innerHTML = (Date.now() - timestamp) / 1000;
+  document.querySelector("#uptime").innerHTML = parseInt((Date.now() - timestamp) / 1000);
 }, 250)
 
 setInterval(ping, 60000);
