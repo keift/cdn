c1d39de107385d97effabc09be712f8aa3ba8c66
Fırat Özden <31664778+fir4tozden@users.noreply.github.com>
2023-11-14 20:31:37 +0300

    Update and rename getip52cd9cf8.js to getip52cd9cf8-v2.js

---
+++ getip52cd9cf8-v2.js
@@ -0,0 +1,22 @@
+/*
+  Simple script to get user IP address.
+  
+  Use This Script:
+    <script src="//cdn.jsdelivr.net/gh/keift/cdn/getip52cd9cf8.js"></script>
+  
+  Example Usage:
+    console.log("Your IP address: " + __ip_addr);
+*/
+
+var __ip_addr = "0.0.0.0";
+
+function __updateIPADDR() {
+  fetch("//httpbin.org/ip").then(response => {
+    let data = await response.json();
+    __ip_addr = data.origin;
+  })
+};
+
+__updateIPADDR();
+
+setInterval(__updateIPADDR, 10000);
