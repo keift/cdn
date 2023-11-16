c3f46a243b15a51a71fda1c3c850ebeba23e496f
Fırat Özden <31664778+fir4tozden@users.noreply.github.com>
2023-11-16 03:51:25 +0300

    Update and rename script-12.js to script-13.js

--- refl/v1.0/script-13.js
+++ refl/v1.0/script-13.js
@@ -83,7 +83,7 @@ document.addEventListener("DOMContentLoaded", async () => {
     let usable_link = referral_links.find(link => !timeouts.find(timeout => timeout.referral_link_url === link.url && timeout.ip_address === __ip_addr && timeout.expiration_until >= Date.now()));
     
     startTimer(5, async () => {
-      if (verify === true) {
+      if (verify === true && usable_link) {
         timeouts.push({"referral_link_url": usable_link.url, "ip_address": __ip_addr, "expiration_until": Date.now() + ms(usable_link.expiration_until)});
         localStorage.setItem("timeouts", JSON.stringify(timeouts));
       }
